2961343f7b900fe0fae53ffb9dc01224976332a1
VitalyPeryatin <peryatin.vitalik37@gmail.com>
2020-01-05 04:19:54 +0300

    Add Edit Text to ChatFragment

--- app/src/main/java/com/tla/uchattut/presentation/chat/view/ChatFragment.kt
+++ app/src/main/java/com/tla/uchattut/presentation/chat/view/ChatFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
